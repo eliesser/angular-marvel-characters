1764e7fa757b046f5d0b5ad680575d2ef5fb1496
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-12 00:47:12 +0200

    add card and list characters

--- src/app/components/character-card/character-card.component.ts
+++ src/app/components/character-card/character-card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+import { ICharacter } from '../../interfaces/character.interface';
+import { FavoriteButtonComponent } from '../favorite-button/favorite-button.component';
 
 @Component({
   selector: 'app-character-card',
   standalone: true,
-  imports: [],
+  imports: [NgIf, FavoriteButtonComponent],
   templateUrl: './character-card.component.html',
-  styleUrl: './character-card.component.scss'
+  styleUrl: './character-card.component.scss',
 })
 export class CharacterCardComponent {
-
+  @Input() character!: ICharacter;
 }
--- src/app/components/characters-list/characters-list.component.ts
+++ src/app/components/characters-list/characters-list.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 
 import { CharacterService } from '../../services/character/character.service';
 import { ICharacter, IResults } from '../../interfaces/character.interface';
+import { CharacterCardComponent } from '../character-card/character-card.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-characters-list',
   standalone: true,
-  imports: [],
+  imports: [CharacterCardComponent, NgFor],
   templateUrl: './characters-list.component.html',
   styleUrl: './characters-list.component.scss',
 })
