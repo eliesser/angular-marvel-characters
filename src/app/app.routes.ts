fb771aef659ef74aa081a0e7ceac07702480e7b3
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-11 22:23:25 +0200

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'characters-list',
+    loadComponent: () =>
+      import('./components/characters-list/characters-list.component').then(
+        (c) => c.CharactersListComponent
+      ),
+  },
+  {
+    path: 'character/:id',
+    loadComponent: () =>
+      import('./components/character-detail/character-detail.component').then(
+        (c) => c.CharacterDetailComponent
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'characters-list',
+    pathMatch: 'full',
+  },
+];
